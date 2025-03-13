7d563b3785398fd68e600bb3cc44ee0ea3df0fcd
MrScript <andrea.marucci.perso@gmail.com>
2025-03-13 21:01:09 +0100

    Add dashboard

---
+++ demo/Dashboard.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace demo
+{
+    partial class frmDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.gestionDesTicketsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDesUtilisateursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenu
+            // 
+            this.msMenu.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.msMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionDesTicketsToolStripMenuItem,
+            this.gestionDesUtilisateursToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(800, 33);
+            this.msMenu.TabIndex = 0;
+            this.msMenu.Text = "menuStrip1";
+            this.msMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // gestionDesTicketsToolStripMenuItem
+            // 
+            this.gestionDesTicketsToolStripMenuItem.Name = "gestionDesTicketsToolStripMenuItem";
+            this.gestionDesTicketsToolStripMenuItem.Size = new System.Drawing.Size(176, 29);
+            this.gestionDesTicketsToolStripMenuItem.Text = "Gestion des tickets";
+            this.gestionDesTicketsToolStripMenuItem.Click += new System.EventHandler(this.gestionDesTicketsToolStripMenuItem_Click);
+            // 
+            // gestionDesUtilisateursToolStripMenuItem
+            // 
+            this.gestionDesUtilisateursToolStripMenuItem.Name = "gestionDesUtilisateursToolStripMenuItem";
+            this.gestionDesUtilisateursToolStripMenuItem.Size = new System.Drawing.Size(210, 29);
+            this.gestionDesUtilisateursToolStripMenuItem.Text = "Gestion des utilisateurs";
+            this.gestionDesUtilisateursToolStripMenuItem.Click += new System.EventHandler(this.gestionDesUtilisateursToolStripMenuItem_Click);
+            // 
+            // frmDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.msMenu);
+            this.MainMenuStrip = this.msMenu;
+            this.Name = "frmDashboard";
+            this.Text = "Dashboard";
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem gestionDesTicketsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDesUtilisateursToolStripMenuItem;
+    }
+}
---
+++ demo/Dashboard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo
+{
+    public partial class frmDashboard: Form
+    {
+        public frmDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void gestionDesUtilisateursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmManageUsers manageUsers = new frmManageUsers();
+            this.Hide();
+            manageUsers.ShowDialog();
+            this.Show();
+        }
+
+        private void gestionDesTicketsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmManageTicket manageTickets = new frmManageTicket();
+            this.Hide();
+            manageTickets.ShowDialog();
+            this.Show();
+        }
+    }
+}
